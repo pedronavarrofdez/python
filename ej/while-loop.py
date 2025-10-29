d7bfe1cec810a5183f3d073eeec6e75b4fa1fbbb
Pedro Navarro <pedronavarrofdez@gmail.com>
2025-10-29 20:56:12 +0100

    Create while-loop.py

---
+++ ej/while-loop.py
@@ -0,0 +1,8 @@
+# While loop dispositivos conectados
+max_devices = 5
+i = 1
+
+while i < max_devices:
+  print("El usuario puede conectar dispositivos adicionales")
+  i = i + 1 
+print("Has alcanzado el máximo numero de dispositivos conectados")
